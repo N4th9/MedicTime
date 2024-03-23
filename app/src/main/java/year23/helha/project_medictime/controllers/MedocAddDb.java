60acb60124bcf3dd99aafb6b5370a02edd8bcb27
N4th9 <128613887+N4th9@users.noreply.github.com>
2024-03-23 09:38:05 +0100

    Affichage de l'ajout d'un médicament refait

--- app/src/main/java/year23/helha/project_medictime/controllers/MedocAddDb.java
+++ app/src/main/java/year23/helha/project_medictime/controllers/MedocAddDb.java
@@ -4,11 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,7 +16,7 @@ import year23.helha.project_medictime.models.MedocDb;
 
 public class MedocAddDb extends AppCompatActivity {
 
-    protected Medoc medoc;
+    private Medoc medoc;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,23 +24,23 @@ public class MedocAddDb extends AppCompatActivity {
         setContentView(R.layout.add_medoc_db);
         NewMedocToDb();
 
-        Button mValiderMedocToDb = findViewById(R.id.Confirm_New_Medoc_To_Db);
+        Button mValidateMedocToDb = findViewById(R.id.Confirm_New_Medoc_To_Db);
 
-        mValiderMedocToDb.setOnClickListener(v -> {
+        mValidateMedocToDb.setOnClickListener(v -> {
             MedocDb.get(this).addMedoc(medoc);
             Intent intent = new Intent(MedocAddDb.this, MedocAddListView.class);
             startActivity(intent);
         });
 
-        EditText mName = findViewById(R.id.TextToDb);
-        EditText mDuree = this.findViewById(R.id.DureeToDb);
-        CheckBox CheckBoxMatin = this.findViewById(R.id.CheckBox_Matin_Db);
+        EditText mName = this.findViewById(R.id.TextToDb);
+        EditText mDuration = this.findViewById(R.id.DureeToDb);
+        CheckBox CheckBoxMorning = this.findViewById(R.id.CheckBox_Matin_Db);
         CheckBox CheckBoxMidi = this.findViewById(R.id.CheckBox_Midi_Db);
-        CheckBox CheckBoxSoir = this.findViewById(R.id.CheckBox_Soir_Db);
+        CheckBox CheckBoxEvening = this.findViewById(R.id.CheckBox_Soir_Db);
 
         medoc = new Medoc();
 
-        InfosMedoc(mName, mDuree, CheckBoxMatin, CheckBoxMidi, CheckBoxSoir);
+        MedocInfo(mName, mDuration, CheckBoxMorning, CheckBoxMidi, CheckBoxEvening);
     }
 
     private void NewMedocToDb() {
@@ -50,7 +48,7 @@ public class MedocAddDb extends AppCompatActivity {
         medoc.setmName("");
     }
 
-    private void InfosMedoc(EditText mName, EditText mDuree, CheckBox CheckBoxMatin, CheckBox CheckBoxMidi, CheckBox CheckBoxSoir) {
+    private void MedocInfo(EditText mName, EditText mDuration, CheckBox CheckBoxMorning, CheckBox CheckBoxMidi, CheckBox CheckBoxEvening) {
         mName.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -65,7 +63,7 @@ public class MedocAddDb extends AppCompatActivity {
             public void afterTextChanged(Editable s) {
             }
         });
-        mDuree.addTextChangedListener(new TextWatcher() {
+        mDuration.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
@@ -79,8 +77,8 @@ public class MedocAddDb extends AppCompatActivity {
             public void afterTextChanged(Editable s) {
             }
         });
-        CheckBoxMatin.setOnClickListener(v -> medoc.setMatin(CheckBoxMatin.isChecked()));
+        CheckBoxMorning.setOnClickListener(v -> medoc.setMatin(CheckBoxMorning.isChecked()));
         CheckBoxMidi.setOnClickListener(v -> medoc.setMidi(CheckBoxMidi.isChecked()));
-        CheckBoxSoir.setOnClickListener(v -> medoc.setSoir(CheckBoxSoir.isChecked()));
+        CheckBoxEvening.setOnClickListener(v -> medoc.setSoir(CheckBoxEvening.isChecked()));
     }
 }
